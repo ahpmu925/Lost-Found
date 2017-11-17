9d8fee97d574a6e10aedda1b5ceac42ecfba1e7f
ahpmu925 <33218588+ahpmu925@users.noreply.github.com>
2017-11-17 15:58:04 -0800

    C# Domain

---
+++ ASP.NET/FoundItem.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostandFound.Models.Domain
+{
+    public class FoundItem
+    {
+
+        public string Item { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+    }
+}
